965d3a481c0f55c33ceda5b93dd0c5ca48f668df
J4ckIsB4ck <sans15615@gmail.com>
2024-05-01 17:37:38 +0300

    About Us page modified

--- src/pages/AboutUs/AboutUs.jsx
+++ src/pages/AboutUs/AboutUs.jsx
@@ -1,4 +1,4 @@
-import { Box } from "@mui/material";
+import Icon from '@mui/material/Icon';
 import Navbar from "../../components/Navbar";
 import Footer from "../../components/Footer";
 import  "./AboutUs.css";
@@ -7,9 +7,54 @@ function AboutUs() {
     return (
       <>
         <Navbar/>
-          <Box className="box-about">
-            <h1>About Us Page</h1>
-          </Box>
+          <div className="container-about">
+            <div className="header-about">
+              <h1>Our Team</h1>
+            </div>
+            <div className="sub-container-about">
+            <div className="teams-about">
+                <img src="Person_one_team.jpg" alt="prs_one_team" />
+                <div className="name">Person name</div>
+                <div className="desig">Developer</div>
+                <div className="about">Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+                   Architecto harum laborum voluptate reiciendis animi tempore deleniti illo omnis saepe. Perferendis.</div>
+                   <div className="social-links">
+                      <a href="#"><Icon className="fab fa-facebook" /></a>
+                      <a href="#"><Icon className="fab fa-instagram" /></a>
+                      <a href="#"><Icon className="fab fa-twitter" /></a>
+                      <a href="#"><Icon className="fab fa-github" /></a>
+                   </div>
+            </div>
+
+            <div className="teams-about">
+                <img src="Person_two_team.jpg" alt="prs_two_team" />
+                <div className="name">Person name</div>
+                <div className="desig">Designer</div>
+                <div className="about">Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+                   Architecto harum laborum voluptate reiciendis animi tempore deleniti illo omnis saepe. Perferendis.</div>
+                   <div className="social-links">
+                      <a href="#"><Icon className="fab fa-facebook" /></a>
+                      <a href="#"><Icon className="fab fa-instagram" /></a>
+                      <a href="#"><Icon className="fab fa-twitter" /></a>
+                      <a href="#"><Icon className="fab fa-github" /></a>
+                   </div>
+            </div>
+
+            <div className="teams-about">
+                <img src="Person_three_team.jpg" alt="prs_three_team" />
+                <div className="name">Person name</div>
+                <div className="desig">Manager</div>
+                <div className="about">Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+                   Architecto harum laborum voluptate reiciendis animi tempore deleniti illo omnis saepe. Perferendis.</div>
+                   <div className="social-links">
+                      <a href="#"><Icon className="fab fa-facebook" /></a>
+                      <a href="#"><Icon className="fab fa-instagram" /></a>
+                      <a href="#"><Icon className="fab fa-twitter" /></a>
+                      <a href="#"><Icon className="fab fa-github" /></a>
+                   </div>
+            </div>
+            </div>
+          </div>
         <Footer/>
       </>
     );
