c69482657f23eb58b96fd8b604c13edeca85baef
J4ckIsB4ck <sans15615@gmail.com>
2024-05-08 22:34:09 +0300

    Added some changes on blog page

--- src/pages/Blog/Blog.jsx
+++ src/pages/Blog/Blog.jsx
@@ -1,15 +1,95 @@
-import { Box } from "@mui/material";
 import Navbar from "../../components/Navbar";
 import Footer from "../../components/Footer";
+import Icon from '@mui/material/Icon';
 import  "./Blog.css";
 
 function Blog() {
     return (
       <>
         <Navbar/>
-          <Box className="box-blog">
-            <h1>Blog Page</h1>
-        </Box>
+          <div className="blog-back">
+            <div className="blog-content">
+              <div className="blog-container">
+                <h1 className="blog-title">
+                  <span>The QuickB Blog</span>
+                </h1>
+                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+                  Alias eaque, aperiam ullam minima aut tenetur.</p>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+              </div>
+            </div>
+          </div>
+
+          <div className="blog-design">
+            <div className="blog-container">
+              <div className="title">
+                <h2>Some text</h2>
+                <p>Lorem ipsum dolor sit amet.</p>
+              </div>
+
+              <div className="blog-design-content">
+                <div className="blog-design-item">
+                  <div className="blog-design-img">
+                    <img src="test_image.jpg" alt="" />
+                    <Icon className="far fa-heart"/>
+                    <span>Lorem, ipsum dolor.</span>
+                  </div>
+                  <div className="blog-design-title">
+                    <a href="#">Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+                    Cumque consectetur laborum magnam.</a>
+                  </div>
+                </div>
+
+                <div className="blog-design-item">
+                  <div className="blog-design-img">
+                    <img src="test_image.jpg" alt="" />
+                    <Icon className="far fa-heart"/>
+                    <span>Lorem, ipsum dolor.</span>
+                  </div>
+                  <div className="blog-design-title">
+                    <a href="#">Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+                    Cumque consectetur laborum magnam.</a>
+                  </div>
+                </div>
+                
+                <div className="blog-design-item">
+                  <div className="blog-design-img">
+                    <img src="test_image.jpg" alt="" />
+                    <Icon className="far fa-heart"/>
+                    <span>Lorem, ipsum dolor.</span>
+                  </div>
+                  <div className="blog-design-title">
+                    <a href="#">Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+                    Cumque consectetur laborum magnam.</a>
+                  </div>
+                </div>
+
+                <div className="blog-design-item">
+                  <div className="blog-design-img">
+                    <img src="test_image.jpg" alt="" />
+                    <Icon className="far fa-heart"/>
+                    <span>Lorem, ipsum dolor.</span>
+                  </div>
+                  <div className="blog-design-title">
+                    <a href="#">Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+                    Cumque consectetur laborum magnam.</a>
+                  </div>
+                </div>
+
+                <div className="blog-design-item">
+                  <div className="blog-design-img">
+                    <img src="test_image.jpg" alt="" />
+                    <Icon className="far fa-heart"/>
+                    <span>Lorem, ipsum dolor.</span>
+                  </div>
+                  <div className="blog-design-title">
+                    <a href="#">Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+                    Cumque consectetur laborum magnam.</a>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
         <Footer/>
       </>
     );
