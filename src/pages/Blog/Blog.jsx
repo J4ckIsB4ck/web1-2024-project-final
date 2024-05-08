bb751c5304c969c865f078f7e2a32dc7deb5ca56
J4ckIsB4ck <sans15615@gmail.com>
2024-05-08 23:23:33 +0300

    Added some style to blog page

--- src/pages/Blog/Blog.jsx
+++ src/pages/Blog/Blog.jsx
@@ -30,7 +30,7 @@ function Blog() {
               <div className="blog-design-content">
                 <div className="blog-design-item">
                   <div className="blog-design-img">
-                    <img src="test_image.jpg" alt="" />
+                    <img className="img-blog" src="test_image.jpg" alt="" />
                     <Icon className="far fa-heart"/>
                     <span>Lorem, ipsum dolor.</span>
                   </div>
@@ -42,7 +42,7 @@ function Blog() {
 
                 <div className="blog-design-item">
                   <div className="blog-design-img">
-                    <img src="test_image.jpg" alt="" />
+                    <img className="img-blog" src="test_image.jpg" alt=""/>
                     <Icon className="far fa-heart"/>
                     <span>Lorem, ipsum dolor.</span>
                   </div>
@@ -54,7 +54,7 @@ function Blog() {
                 
                 <div className="blog-design-item">
                   <div className="blog-design-img">
-                    <img src="test_image.jpg" alt="" />
+                    <img className="img-blog" src="test_image.jpg" alt="" />
                     <Icon className="far fa-heart"/>
                     <span>Lorem, ipsum dolor.</span>
                   </div>
@@ -66,7 +66,7 @@ function Blog() {
 
                 <div className="blog-design-item">
                   <div className="blog-design-img">
-                    <img src="test_image.jpg" alt="" />
+                    <img className="img-blog" src="test_image.jpg" alt="" />
                     <Icon className="far fa-heart"/>
                     <span>Lorem, ipsum dolor.</span>
                   </div>
@@ -78,13 +78,104 @@ function Blog() {
 
                 <div className="blog-design-item">
                   <div className="blog-design-img">
-                    <img src="test_image.jpg" alt="" />
+                    <img className="img-blog" src="test_image.jpg" alt="" />
                     <Icon className="far fa-heart"/>
                     <span>Lorem, ipsum dolor.</span>
                   </div>
                   <div className="blog-design-title">
                     <a href="#">Lorem ipsum, dolor sit amet consectetur adipisicing elit.
                     Cumque consectetur laborum magnam.</a>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div className="latest-blog">
+            <div className="blog-container">
+              <div className="title">
+                <h2>Latest Blog</h2>
+                <p>recent blogs about quiz</p>
+              </div>
+              <div className="blog-latest-content">
+
+                <div className="blog-item">
+                  <div className="blog-img">
+                    <img className="img-blog" src="test_image.jpg" alt="" />
+                    <Icon className="far fa-heart"/>
+                  </div>
+                  <div className="blog-text">
+                    <span>20 may, 2024</span>
+                    <h2>Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                       Natus rerum illo culpa.</h2>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                       Quas velit distinctio corrupti laudantium expedita dolorem assumenda,
+                       eius ducimus natus quibusdam.</p>
+                    <a href="#">Read more</a>
+                  </div>
+                </div>
+
+                <div className="blog-item">
+                  <div className="blog-img">
+                    <img className="img-blog" src="test_image.jpg" alt="" />
+                    <Icon className="far fa-heart"/>
+                  </div>
+                  <div className="blog-text">
+                    <span>20 may, 2024</span>
+                    <h2>Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                       Natus rerum illo culpa.</h2>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                       Quas velit distinctio corrupti laudantium expedita dolorem assumenda,
+                       eius ducimus natus quibusdam.</p>
+                    <a href="#">Read more</a>
+                  </div>
+                </div>
+
+                <div className="blog-item">
+                  <div className="blog-img">
+                    <img className="img-blog" src="test_image.jpg" alt="" />
+                    <Icon className="far fa-heart"/>
+                  </div>
+                  <div className="blog-text">
+                    <span>20 may, 2024</span>
+                    <h2>Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                       Natus rerum illo culpa.</h2>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                       Quas velit distinctio corrupti laudantium expedita dolorem assumenda,
+                       eius ducimus natus quibusdam.</p>
+                    <a href="#">Read more</a>
+                  </div>
+                </div>
+
+                <div className="blog-item">
+                  <div className="blog-img">
+                    <img className="img-blog" src="test_image.jpg" alt="" />
+                    <Icon className="far fa-heart"/>
+                  </div>
+                  <div className="blog-text">
+                    <span>20 may, 2024</span>
+                    <h2>Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                       Natus rerum illo culpa.</h2>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                       Quas velit distinctio corrupti laudantium expedita dolorem assumenda,
+                       eius ducimus natus quibusdam.</p>
+                    <a href="#">Read more</a>
+                  </div>
+                </div>
+
+                <div className="blog-item">
+                  <div className="blog-img">
+                    <img className="img-blog" src="test_image.jpg" alt="" />
+                    <Icon className="far fa-heart"/>
+                  </div>
+                  <div className="blog-text">
+                    <span>20 may, 2024</span>
+                    <h2>Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                       Natus rerum illo culpa.</h2>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                       Quas velit distinctio corrupti laudantium expedita dolorem assumenda,
+                       eius ducimus natus quibusdam.</p>
+                    <a href="#">Read more</a>
                   </div>
                 </div>
               </div>
